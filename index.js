d4332802370d220caaff3de316789abfceab5d88
Rejunraj <rejunrajreju@gmail.com>
2021-11-27 18:03:00 +0530

    ⏪ revert change

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
 const port = process.env.PORT || 3010;
 
 // sendFile will go here
-app.get('/test', function(req, res) {
-    res.send({ message: 'awesome!!' })
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
